1966d822aa82d7673753cc23e63a864d5e2789e1
DaniFoldi <dani@stefan.hu>
2022-04-16 22:59:09 +0200

    :hammer: Fix vue preset special functions

--- stylelint-presets/vue/index.js
+++ stylelint-presets/vue/index.js
@@ -5,5 +5,9 @@ module.exports = {
       extends: [ '@danifoldi/stylelint-presets/standard' ],
       files: [ '**/*.css', '*.css' ]
     }
-  ]
+  ],
+  rules: {
+    'function-no-unknown': [ true, { ignoreFunctions: [ 'v-bind' ] }],
+    'selector-pseudo-element-no-unknown': [ true, { ignorePseudoElements: [ 'v-deep' ] }]
+  }
 }
