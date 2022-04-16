af73c09d718a59e232c85561666562010afc1c49
DaniFoldi <dani@stefan.hu>
2022-04-16 22:54:11 +0200

    :hammer: [ci release] Fix vue-scss preset

--- stylelint-presets/vue-scss/index.js
+++ stylelint-presets/vue-scss/index.js
@@ -1,9 +1,12 @@
 module.exports = {
-  extends: [ '@danifoldi/stylelint-presets/scss', '@danifoldi/stylelint-presets/vue' ],
+  extends: [ 'stylelint-config-recommended-vue/scss', '@danifoldi/stylelint-presets/scss', '@danifoldi/stylelint-presets/vue' ],
   overrides: [
     {
       extends: [ '@danifoldi/stylelint-presets/scss' ],
       files: [ '**/*.scss', '*.scss' ]
     }
-  ]
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+  }
 }
