e5e092e35a97886aeb1469111a6d57ca26832427
DaniFoldi <dani@stefan.hu>
2022-04-17 14:02:12 +0200

    :hammer: Fix vue preset issues

--- stylelint-presets/vue-scss/index.js
+++ stylelint-presets/vue-scss/index.js
@@ -3,12 +3,12 @@ module.exports = {
   overrides: [
     {
       customSyntax: 'postcss-html',
-      files: [ '*.vue', '**/*.vue' ],
       extends: [ 'stylelint-config-recommended-scss' ],
+      files: [ '*.vue', '**/*.vue' ],
       rules: {
         'function-no-unknown': [ true, { ignoreFunctions: [ 'v-bind' ] }],
-        'selector-pseudo-element-no-unknown': [ true, { ignorePseudoElements: [ 'v-deep' ] }],
-        'selector-pseudo-class-no-unknown': [ true, { ignorePseudoClasses: [ 'deep', 'global' ] }]
+        'selector-pseudo-class-no-unknown': [ true, { ignorePseudoClasses: [ 'deep', 'global' ] }],
+        'selector-pseudo-element-no-unknown': [ true, { ignorePseudoElements: [ 'v-deep', 'v-global', 'v-slotted' ] }]
       }
     },
     {
--- stylelint-presets/vue/index.js
+++ stylelint-presets/vue/index.js
@@ -7,8 +7,8 @@ module.exports = {
       files: [ '*.vue', '**/*.vue' ],
       rules: {
         'function-no-unknown': [ true, { ignoreFunctions: [ 'v-bind' ] }],
-        'selector-pseudo-element-no-unknown': [ true, { ignorePseudoElements: [ 'v-deep', 'v-global', 'v-slotted' ] }],
-        'selector-pseudo-class-no-unknown': [ true, { ignorePseudoClasses: [ 'deep', 'global' ] }]
+        'selector-pseudo-class-no-unknown': [ true, { ignorePseudoClasses: [ 'deep', 'global' ] }],
+        'selector-pseudo-element-no-unknown': [ true, { ignorePseudoElements: [ 'v-deep', 'v-global', 'v-slotted' ] }]
       }
     },
     {
