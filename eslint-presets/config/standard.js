ab1546fdb96831a7177c47e9b219dac04873e912
DaniFoldi <dani@stefan.hu>
2022-04-27 18:25:37 +0100

    :hammer: FIx some aesthetic issues

--- eslint-presets/config/standard.js
+++ eslint-presets/config/standard.js
@@ -133,8 +133,8 @@ module.exports = {
     'object-curly-newline': [
       'warn',
       {
-        ExportDeclaration: { minProperties: 3, multiline: true },
-        ImportDeclaration: 'never'
+        ExportDeclaration: { minProperties: 6, multiline: true },
+        ImportDeclaration: { minProperties: 6, multiline: true }
       }
     ],
     'object-curly-spacing': [ 'warn', 'always' ],
@@ -146,7 +146,7 @@ module.exports = {
     'quotes': [ 'warn', 'single' ],
     'semi': [ 'warn', 'never' ],
     'sort-imports': 'off',
-    'sort-keys': [ 'warn', 'asc', { caseSensitive: false, minKeys: 5, natural: true }],
+    'sort-keys': [ 'warn', 'asc', { caseSensitive: false, minKeys: 6, natural: true }],
     'space-before-function-paren': [ 'error', { anonymous: 'never', asyncArrow: 'always', named: 'never' }],
     'space-in-parens': [ 'warn', 'never' ],
     'space-infix-ops': 'warn',
