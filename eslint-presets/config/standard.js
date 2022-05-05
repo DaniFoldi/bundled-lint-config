ecf31e2e4127eaac01d49cc8b6c73d6d03f45616
DaniFoldi <dani@stefan.hu>
2022-05-05 19:06:53 +0100

    :zap: Allow foreach because it is nice

--- eslint-presets/config/standard.js
+++ eslint-presets/config/standard.js
@@ -154,6 +154,7 @@ module.exports = {
     'template-curly-spacing': [ 'warn', 'never' ],
     'unicorn/filename-case': [ 'warn', { cases: { camelCase: true, kebabCase: true, pascalCase: true } }],
     'unicorn/no-array-callback-reference': 'off',
+    'unicorn/no-array-for-each': 'off',
     'unicorn/no-array-reduce': 'off',
     'unicorn/no-null': 'off',
     'unicorn/numeric-separators-style': [ 'warn', { onlyIfContainsSeparator: true }],
