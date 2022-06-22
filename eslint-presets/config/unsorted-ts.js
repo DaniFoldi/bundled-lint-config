b0925183179ac57efb2e39b17e8500bd4c7cff3c
DaniFoldi <dani@stefan.hu>
2022-06-22 15:53:20 +0200

    :zap: Add unsorted and unsorted-ts presets

---
+++ eslint-presets/config/unsorted-ts.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [ 'plugin:@danifoldi/eslint-presets/ts' ],
+  rules: {
+    '@typescript-eslint/member-ordering': 'off'
+  }
+}
--- eslint-presets/index.js
+++ eslint-presets/index.js
@@ -16,6 +16,8 @@ module.exports = {
     'standard': require('@danifoldi/eslint-plugin-eslint-presets/config/standard.js'),
     'ts': require('@danifoldi/eslint-plugin-eslint-presets/config/ts.js'),
     'ts-jest': require('@danifoldi/eslint-plugin-eslint-presets/config/ts-jest.js'),
+    'unsorted': require('@danifoldi/eslint-plugin-eslint-presets/config/unsorted.ts'),
+    'unsorted-ts': require('@danifoldi/eslint-plugin-eslint-presets/config/unsorted-ts.ts'),
     'vitest': require('@danifoldi/eslint-plugin-eslint-presets/config/vitest.js'),
     'vue': require('@danifoldi/eslint-plugin-eslint-presets/config/vue.js'),
     'vue-ts': require('@danifoldi/eslint-plugin-eslint-presets/config/vue-ts.js')
