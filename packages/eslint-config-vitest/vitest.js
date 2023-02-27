579118bdfbde38cd64e4d7c5e821f8a06376e9f3
Dániel Földi <dani@stefan.hu>
2023-02-27 17:39:19 +0000

    :zap: Update vitest package

--- packages/eslint-config-vitest/vitest.js
+++ packages/eslint-config-vitest/vitest.js
@@ -2,13 +2,20 @@ module.exports = {
   extends: [ './standard.js' ],
   plugins: [ 'vitest' ],
   rules: {
+    'vitest/consistent-test-filename': 'warn',
     'vitest/consistent-test-it': 'warn',
     'vitest/expect-expect': 'warn',
-    'vitest/lower-case-title': 'warn',
     'vitest/max-nested-describe': 'warn',
+    'vitest/no-conditional-expect': 'warn',
+    'vitest/no-conditional-in-test': 'warn',
     'vitest/no-conditional-tests': 'warn',
+    'vitest/no-disabled-tests': 'warn',
+    'vitest/no-done-callback': 'warn',
+    'vitest/no-restricted-vi-methods': 'warn',
     'vitest/no-focused-tests': 'warn',
     'vitest/no-identical-title': 'warn',
-    'vitest/no-skipped-tests': 'warn'
+    'vitest/no-skipped-tests': 'warn',
+    'vitest/prefer-lowercase-title': 'warn',
+    'vitest/prefer-to-be': 'warn'
   }
 }
