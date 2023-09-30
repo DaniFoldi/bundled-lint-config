13c76fb26260a6ac56089a12fdc4a5727467dca2
Dániel Földi <dani@stefan.hu>
2023-09-30 19:41:31 +0200

    :hammer: Add node globals

--- packages/bundled-eslint-config/src/setup/for-node.ts
+++ packages/bundled-eslint-config/src/setup/for-node.ts
@@ -1,4 +1,4 @@
-
+import globals from 'globals'
 import type { Globals, LanguageOptions, Plugins, Rules, Settings } from '../util'
 
 
@@ -6,7 +6,9 @@ export const nodeSettings: Settings = {}
 
 export const nodeLanguageOptions: LanguageOptions = {}
 
-export const nodeGlobals: Globals = {}
+export const nodeGlobals: Globals = {
+  ...globals.node
+}
 
 export const nodePlugins: Plugins = {}
 
