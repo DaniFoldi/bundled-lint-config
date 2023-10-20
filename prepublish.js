4374aacb19fb9e17ba6621b3d758a2a5eaf3d24a
Dániel Földi <dani@stefan.hu>
2023-10-20 21:13:57 +0200

    :zap: [ci release] Change severity of some recommendation rules

--- prepublish.js
+++ prepublish.js
@@ -28,7 +28,7 @@ const packageJsonJobs = {
   },
   'packages/bundled-eslint-config': {
     type: 'module',
-    version: '0.2.5',
+    version: '0.2.6',
     description: 'A preset for ESLint',
     keywords: [
       'eslint',
