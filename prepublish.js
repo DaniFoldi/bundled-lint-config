67bce58e56ab1360da92866abd697b35922a14aa
Dániel Földi <dani@stefan.hu>
2023-10-26 15:09:05 +0200

    :octocat: [ci release] Bump version

--- prepublish.js
+++ prepublish.js
@@ -28,7 +28,7 @@ const packageJsonJobs = {
   },
   'packages/bundled-eslint-config': {
     type: 'module',
-    version: '0.2.8',
+    version: '0.2.9',
     description: 'A preset for ESLint',
     keywords: [
       'eslint',
