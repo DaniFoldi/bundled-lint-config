db3c09756fb88d66f38e83405d4014e18591dc94
Dániel Földi <dani@stefan.hu>
2024-02-09 20:40:16 +0100

    :mag: Fix lint issues

--- update.js
+++ update.js
@@ -2,6 +2,6 @@ import { execSync } from 'node:child_process'
 import { globby } from 'globby'
 
 
-for (const dir of await (globby('packages/*', { onlyFiles: false, markDirectories: true }))) {
+for (const dir of await globby('packages/*', { onlyFiles: false, markDirectories: true })) {
   execSync('pnpm update', { cwd: dir })
 }
