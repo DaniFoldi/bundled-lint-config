c02771cb48e4f0912ba8e741f0400919c30be2a5
Dániel Földi <dani@stefan.hu>
2024-03-26 18:11:16 +0100

    :hammer: [ci release] Fix jsdoc

--- prepublish.js
+++ prepublish.js
@@ -24,7 +24,7 @@ const packageJsonJobs = {
   },
   'packages/bundled-eslint-config': {
     type: 'module',
-    version: '0.2.23',
+    version: '0.2.24',
     description: 'A preset for ESLint',
     keywords: [
       'eslint',
