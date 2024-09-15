85bd880c0ef4c76f8c763f961551fe1c61006ab0
Dániel Földi <6977583+DaniFoldi@users.noreply.github.com>
2024-09-15 11:00:50 +0100

    :paperclip: Publish fixes

--- prepublish.js
+++ prepublish.js
@@ -24,7 +24,7 @@ const packageJsonJobs = {
   },
   'packages/bundled-eslint-config': {
     type: 'module',
-    version: '0.4.17',
+    version: '0.4.19',
     description: 'A preset for ESLint',
     keywords: [
       'eslint',
