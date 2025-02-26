38f8ce1771f8035355204cfb354bbd8d4f1eeb8b
Dániel Földi <dani@stefan.hu>
2025-02-26 11:54:58 +0000

    :hammer: [ci release] Silence typecheck

--- packages/bundled-eslint-config/src/setup/for-vitest.ts
+++ packages/bundled-eslint-config/src/setup/for-vitest.ts
@@ -11,6 +11,8 @@ export const vitestLanguageOptions: LanguageOptions = {
 }
 
 export const vitestPlugins: Plugins = {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore sometimes causes issues
   vitest: eslintPluginVitest
 }
 
