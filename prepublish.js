6140983376bbe8c30c8e97bcabe0dc4452af7396
Dániel Földi <dani@stefan.hu>
2025-04-23 10:39:03 +0100

    :hammer: [ci release] Fix downstream typecheck issue

--- prepublish.js
+++ prepublish.js
@@ -24,7 +24,7 @@ const packageJsonJobs = {
   },
   'packages/bundled-eslint-config': {
     type: 'module',
-    version: '0.5.18',
+    version: '0.5.19',
     description: 'A preset for ESLint',
     keywords: [
       'eslint',
